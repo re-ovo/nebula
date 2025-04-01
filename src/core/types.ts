27d32f9e426d6620619ec7191aba5da378b778cd
RE <qq1609403959@outlook.com>
2025-04-01 13:52:17 +0800

    feat: export types from core index

---
+++ src/core/types.ts
@@ -0,0 +1,11 @@
+export type TypedArray =
+  | Uint8Array
+  | Uint16Array
+  | Uint32Array
+  | Int8Array
+  | Int16Array
+  | Int32Array
+  | Float32Array
+  | Float64Array
+  | BigInt64Array
+  | BigUint64Array;
